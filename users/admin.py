d73a555091425d6685704d75d54cbf5e8e459076
younh0103 <gkdld1004@gmail.com>
2023-12-12 13:26:27 +0900

    User App

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+    
+    """ Custom User Adimin """
+    
+    # list_display = ('username', 'email', 'gender', 'language', 'currency', 'superhost')
+    # list_filter = ('language', 'currency', 'superhost')
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ("Custom Profile", {"fields": (
+            "avatar", 
+            "gender", 
+            "bio", 
+            "birthdate", 
+            "language", 
+            "currency", 
+            "superhost")
+                            }),)
+    
--- users/models.py
+++ users/models.py
@@ -3,5 +3,29 @@ from django.db import models
 
 class User(AbstractUser):
     
-    bio = models.TextField(default="")
+    """ Custom User Model """
+    
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+    
+    GENDER_CHOICES = ((GENDER_MALE, "male"), (GENDER_FEMALE, "female"), (GENDER_OTHER, "other"))
+    
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+    
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "english"), (LANGUAGE_KOREAN, "korean"))
+    
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+    
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+    
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True)
+    bio = models.TextField(default="" , blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True)
+    superhost = models.BooleanField(default=False)
     
