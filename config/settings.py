8da920214b0fb3581f4417156136c62faebbe0b1
younh0103 <gkdld1004@gmail.com>
2023-12-13 14:11:06 +0900

    All Other Apps !

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,13 @@ DJANGO_APPS = [
 THRID_PARTY_APPS = ['django_countries']
 
 PROJECT_APPS = [
-    'core.apps.CoreConfig', 'users.apps.UsersConfig', 'rooms.apps.RoomsConfig'
+    'core.apps.CoreConfig', 
+    'users.apps.UsersConfig', 
+    'rooms.apps.RoomsConfig',
+    'reviews.apps.ReviewsConfig',
+    'reservations.apps.ReservationsConfig',
+    'lists.apps.ListsConfig',
+    'conversations.apps.ConversationsConfig'
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THRID_PARTY_APPS + PROJECT_APPS
--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Reservaction)
+class ReservationAdmin(admin.ModelAdmin):
+    
+    """ Reservation Admin Definition """
+    
+    pass
