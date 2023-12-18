c153f5f03c52f5761baacaac24585f06d5f6f1be
younh0103 <gkdld1004@gmail.com>
2023-12-18 15:18:00 +0900

    review, reservation admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -38,6 +38,7 @@ class RoomAdmin(admin.ModelAdmin):
         "instant_book",
         "count_amenities",
         "count_photos",
+        "total_rating",
     )
     
     # ordering = ("name", "price", "bedrooms")
--- users/admin.py
+++ users/admin.py
@@ -19,5 +19,20 @@ class CustomUserAdmin(UserAdmin):
             "language", 
             "currency", 
             "superhost")
-                            }),)
+                            }),
+        )
     
+    list_filter = UserAdmin.list_filter + ("superhost",)
+    
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "language",
+        "currency",
+        "superhost",
+        "is_staff",
+        "is_superuser",
+    )
