4f767d7f646d51a1e63616a5e292a145b7458770
younh0103 <gkdld1004@gmail.com>
2023-12-19 11:21:36 +0900

    Configuring User Uploads part two

--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,9 @@ STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'users.User'
 
+MEDIA_ROOT = BASE_DIR / "uploads"
+
+MEDIA_URL = "/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
