87ddb8c4e913077293da389801876ba600685977
younh0103 <gkdld1004@gmail.com>
2023-12-27 21:21:42 +0900

    User login & logout

--- config/urls.py
+++ config/urls.py
@@ -20,9 +20,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('core.urls', namespace="core")),
-    path('rooms/', include('rooms.urls', namespace="rooms")),
-    path('admin/', admin.site.urls),
+    path("", include("core.urls", namespace="core")),
+    path("rooms/", include("rooms.urls", namespace="rooms")),
+    path("users/", include("users.urls", namespace="users")),
+    path("admin/", admin.site.urls),
 ]
 
 if settings.DEBUG:
