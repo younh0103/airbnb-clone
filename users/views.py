8df49af436f92c411e393575f2f99799ef90a2d0
younh0103 <gkdld1004@gmail.com>
2024-01-30 13:00:49 +0900

    User Done !

--- users/views.py
+++ users/views.py
@@ -13,12 +13,11 @@ from django.contrib.messages.views import SuccessMessageMixin
 from . import forms, models, mixins
 
 
-class LoginView(FormView):
+class LoginView(mixins.LoggedOutOnlyView, FormView):
 
     template_name = "users/login.html"
     form_class = forms.LoginForm
-    success_url = reverse_lazy("core:home")
-    
+
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
         password = form.cleaned_data.get("password")
@@ -27,6 +26,13 @@ class LoginView(FormView):
             login(self.request, user)
             messages.success(self.request, f"Welcome back {user.first_name}")
         return super().form_valid(form)
+
+    def get_success_url(self):
+        next_arg = self.request.GET.get("next")
+        if next_arg is not None:
+            return next_arg
+        else:
+            return reverse("core:home")
 
 
 def log_out(request):
